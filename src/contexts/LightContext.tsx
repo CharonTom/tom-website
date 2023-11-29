9a41f0cea0c502c5492bcb971a95c76627110118
Charon <charon.s.tom@gmail.com>
2023-11-29 21:56:55 +0100

    fixing an issues with the dark mode

--- src/contexts/LightContext.tsx
+++ src/contexts/LightContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, ReactNode } from "react";
 
 interface ThemeContextType {
   theme: string;
@@ -11,21 +11,22 @@ export const ThemeContext = createContext<ThemeContextType | undefined>(
 
 const datas = localStorage.getItem("selectedTheme") || "dark";
 
-export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState(datas);
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [theme, setTheme] = useState<string>(datas);
+
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
+
   useEffect(() => {
     window.localStorage.setItem("selectedTheme", theme);
-    document.documentElement.classList.remove("dark");
-    document.documentElement.classList.remove("light");
-    document.documentElement.classList.add(theme);
   }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
+      <div className={theme}>{children}</div>
     </ThemeContext.Provider>
   );
 };
