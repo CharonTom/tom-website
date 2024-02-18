17a8b78b0b4ce7ee5ac11b5537970842b5732e68
Charon <charon.s.tom@gmail.com>
2024-02-18 13:28:51 +0100

    reworking on the dev bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,92 +5,72 @@ import githubLogo from "../assets/logos/github.webp";
 import externeLogo from "../assets/logos/external-link.webp";
 import { Link } from "react-scroll";
 import { motion } from "framer-motion";
+import { GrTechnology } from "react-icons/gr";
 
-import { BsChatSquareText } from "react-icons/bs";
-
-import { FaUserAlt } from "react-icons/fa";
+import { BsChatSquareText, BsBriefcase, BsGear } from "react-icons/bs";
+import { BiUser } from "react-icons/bi";
 
 function Nav() {
   return (
     <nav className="fixed bottom-4 lg:bottom-5 z-20 mx-auto left-1/2 transform -translate-x-1/2">
-      {/* <motion.ul
+      <motion.div
         initial={{ y: 100 }}
         animate={{ y: 0 }}
         transition={{ delay: 1 }}
-        className="border-2 dark:border-gray border-primary rounded-full dark:bg-navbar bg-light-navbar
-        flex justify-around items-center h-[70px] w-full max-w-[430px] mx-auto"
+        className="text-black text-3xl border-2 dark:border-gray border-primary rounded-full dark:bg-navbar bg-light-navbar
+        flex justify-around items-center h-[70px] max-w-[430px] mx-auto"
       >
-        <li className="mx-2 lg:mx-4 w-[40px] transition duration-500 ease-in-out cursor-pointer hover:scale-125">
-          <Link
-            to="hero"
-            spy={true}
-            smooth={true}
-            activeClass="active"
-            duration={500}
-          >
-            <div className="flex justify-center items-end border-2 border-gray rounded-full w-[40px] h-[40px] overflow-hidden">
-              <FaUserAlt className="text-black text-[31px]" />
-            </div>
-          </Link>
-        </li>
-        <li className="mx-2 lg:mx-4 w-[40px] transition duration-500 ease-in-out cursor-pointer hover:scale-125">
-          <Link
-            to="skills"
-            spy={true}
-            smooth={true}
-            activeClass="active"
-            duration={500}
-            offset={25}
-          >
-            <div className="border-2 border-gray rounded-full">
-              <img
-                src={skillLogo}
-                alt="link's logo"
-                className="h-full rounded-full w-[40px] h-[40px] p-[1px]"
-              />
-            </div>
-          </Link>
-        </li>
-        <li className="mx-2 lg:mx-4 w-[40px] transition duration-500 ease-in-out cursor-pointer hover:scale-125">
-          <Link
-            to="portfolio"
-            spy={true}
-            smooth={true}
-            activeClass="active"
-            duration={500}
-            offset={25}
-          >
-            <div className="border-2 border-gray rounded-full overflow-hidden">
-              <img
-                src={portfolioLogo}
-                alt="link's logo"
-                className="h-full w-[40px] h-[40px] p-[2px]"
-              />
-            </div>
-          </Link>
-        </li>
-        <li className="mx-2 lg:mx-4 w-[40px] transition duration-500 ease-in-out cursor-pointer hover:scale-125">
-          <Link
-            to="contact"
-            spy={true}
-            smooth={true}
-            activeClass="active"
-            duration={500}
-            offset={25}
-          >
-            <div className="flex justify-center items-center border-2 border-gray rounded-full w-[40px] h-[40px] overflow-hidden">
-              <BsChatSquareText className="text-black text-3xl p-[1px]" />
-            </div>
-          </Link>
-        </li>
-        <li className="relative w-[40px] mx-2 lg:mx-4 border-2 border-gray rounded-full hover:scale-125 transition duration-500 ease-in-out relative cursor-pointer">
+        <Link
+          className="mx-3 lg:mx-5 cursor-pointer hover:scale-110 transition duration-300 ease-in-out"
+          to="hero"
+          spy={true}
+          smooth={true}
+          activeClass="active"
+          duration={500}
+        >
+          <BiUser />
+        </Link>
+        <Link
+          className="mx-3 lg:mx-5 cursor-pointer hover:scale-110 transition duration-300 ease-in-out"
+          to="skills"
+          spy={true}
+          smooth={true}
+          activeClass="active"
+          duration={500}
+          offset={25}
+        >
+          <BsGear />
+        </Link>
+        <Link
+          className="mx-3 lg:mx-5 cursor-pointer hover:scale-110 transition duration-300 ease-in-out"
+          to="portfolio"
+          spy={true}
+          smooth={true}
+          activeClass="active"
+          duration={500}
+          offset={25}
+        >
+          <BsBriefcase />
+        </Link>
+        <Link
+          className="mx-3 lg:mx-5 cursor-pointer hover:scale-110 transition duration-300 ease-in-out"
+          to="contact"
+          spy={true}
+          smooth={true}
+          activeClass="active"
+          duration={500}
+          offset={25}
+        >
+          <BsChatSquareText />
+        </Link>
+        <div className="relative w-[36px] mx-2 lg:mx-4 rounded-full hover:scale-110 transition duration-300 ease-in-out cursor-pointer">
           <a
             href="https://github.com/CharonTom"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img
-              className="h-full rounded-full w-[40px] h-[40px]"
+              className="h-full rounded-full w-[36px] h-[36px]"
               src={githubLogo}
               alt="external link pictogram"
             />
@@ -100,15 +80,15 @@ function Nav() {
               alt="external arrow"
             />
           </a>
-        </li>
-        <li className="relative w-[40px] mx-2 lg:mx-4 border-2 border-gray rounded-full hover:scale-125 transition duration-500 ease-in-out relative cursor-pointer">
+        </div>
+        <div className="relative w-[36px] mx-2 lg:mx-4 rounded-full hover:scale-110 transition duration-300 ease-in-out cursor-pointer">
           <a
             href="https://www.linkedin.com/in/tom-charon/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img
-              className="h-full rounded-full w-[40px] h-[40px]"
+              className="h-full rounded-full w-[36px] h-[36px]"
               src={linkedinLogo}
               alt="external link pictogram"
             />
@@ -118,9 +98,8 @@ function Nav() {
               alt="external arrow"
             />
           </a>
-        </li>
-      </motion.ul> */}
-      navbar test
+        </div>
+      </motion.div>
     </nav>
   );
 }
